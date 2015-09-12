ec5c71e0a54a2706f898a6edb56d186dfb69d6c5
Alex Cevallos <Alex.Cevallos@alaskaair.com>
2015-09-11 20:18:09 -0700

    - Added mock data to playgrounds

--- FunFacts/Scratchpad.playground/Contents.swift
+++ FunFacts/Scratchpad.playground/Contents.swift
@@ -3,3 +3,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+//someConstant = 15
+
+//var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("Bananas")
+
+//Optionals
+var optionalString : String?
+println(optionalString)
